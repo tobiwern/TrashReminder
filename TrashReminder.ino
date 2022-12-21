f78b945676b7be0f320c7d48ddbabe07bf632f86
tobiwern <tobiwern@gmail.com>
2022-12-21 10:09:49 +0100

    TestLEDs,WifiManager, Tasks+, ColorHex,15-8,FastLed

--- TrashReminder.ino
+++ TrashReminder.ino
@@ -3,6 +3,7 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <credentials.h>
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
 std::unordered_map<int,int>epochTaskDict = {
 {1685750400,3},
@@ -72,112 +73,75 @@ std::unordered_map<int,int>epochTaskDict = {
 {1684108800,1},
 {1685404800,1},
 {1691366400,1},
+{1672099200,1}, //2022.12.27
+{1672444800,3}, //2022.12.31
+{1671494400,1}, //dummy
 };
-task[] = {"Altpapier-Tonne in Hirrlingen", "Gelber Sack in Hirrlingen", "Häckselgut in Hirrlingen", "Restmüll in Hirrlingen"};
+const String task[] = {"Altpapier-Tonne in Hirrlingen", "Gelber Sack in Hirrlingen", "Häckselgut in Hirrlingen", "Restmüll in Hirrlingen"};
 //         yellow, green, white
-String color[] = {"#0000FF", "#FFFF00", "#00FF00", "#FFFFFF" };
+const int color[] = {0x0000FF, 0xFFFF00, 0x00FF00, 0xFFFFFF };
+const int validIndex[] = {0,1,2,3};
+int startTime = 15*60*60; //seconds => 15 Uhr
+int endTime = (24+8)*60*60; //seconds => nächster Tag 8 Uhr morgens
 
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-date-time-ntp-client-server-arduino/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
-
-
-// Replace with your network credentials
-const char *ssid     = mySSID;
-const char *password = myPASSWORD;
+#include <FastLED.h>
+#define NUM_LEDS 1
+#define DATA_PIN 4 //D2
+CRGB leds[NUM_LEDS];
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-//Week Days
-String weekDays[7]={"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-//Month names
-String months[12]={"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
-  
-  // Connect to Wi-Fi
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-// Initialize a NTPClient to get time
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
+   WiFiManager wm; 
+//   wm.resetSettings();
+   if(wm.autoConnect("TrashReminder")){
+     Serial.println("Successfully connected.");     
+   } else {
+     Serial.println("Failed to connect.");
+   }
   timeClient.begin();
-  // Set offset time in seconds to adjust for your timezone, for example:
-  // GMT +1 = 3600
-  // GMT +8 = 28800
-  // GMT -1 = -3600
-  // GMT 0 = 0
-  timeClient.setTimeOffset(3600);
+  timeClient.setTimeOffset(3600); //GMT+1
 }
 
 void loop() {
   timeClient.update();
+  int nowEpoch = timeClient.getEpochTime();
+//  handleLed(nowEpoch);
+  testLeds();
+}
 
-  time_t epochTime = timeClient.getEpochTime();
-  Serial.print("Epoch Time: ");
-  Serial.println(epochTime);
-  
-  String formattedTime = timeClient.getFormattedTime();
-  Serial.print("Formatted Time: ");
-  Serial.println(formattedTime);  
+void handleLed(int nowEpoch){
+  int dictEpoch;
+  int taskId = -1;
+  for (auto entry :epochTaskDict)  {
+    dictEpoch = entry.first;
+//    Serial.println("now: " + String(nowEpoch) + ", dictEpoch = " + String(dictEpoch) + ", dictEpoch+startTime = " + String(dictEpoch+startTime)+ ", dictEpoch+endTime = " + String(dictEpoch+endTime));
+    if((nowEpoch > dictEpoch+startTime) && (nowEpoch < dictEpoch+endTime)){
+      taskId = entry.second;
+      if()
+    }    
+  }
+  if(taskId != -1){
+    Serial.println("Event: " + task[taskId] + " => LED = " + String(color[taskId]));
+    leds[0] = color[taskId];
+  } else {
+    Serial.println("No Event");
+    leds[0] = CRGB::Black;
+  }
+  FastLED.show();
+  delay(60000); // 1 minute
+}
 
-  int currentHour = timeClient.getHours();
-  Serial.print("Hour: ");
-  Serial.println(currentHour);  
-
-  int currentMinute = timeClient.getMinutes();
-  Serial.print("Minutes: ");
-  Serial.println(currentMinute); 
-   
-  int currentSecond = timeClient.getSeconds();
-  Serial.print("Seconds: ");
-  Serial.println(currentSecond);  
-
-  String weekDay = weekDays[timeClient.getDay()];
-  Serial.print("Week Day: ");
-  Serial.println(weekDay);    
-
-  //Get a time structure
-  struct tm *ptm = gmtime ((time_t *)&epochTime); 
-
-  int monthDay = ptm->tm_mday;
-  Serial.print("Month day: ");
-  Serial.println(monthDay);
-
-  int currentMonth = ptm->tm_mon+1;
-  Serial.print("Month: ");
-  Serial.println(currentMonth);
-
-  String currentMonthName = months[currentMonth-1];
-  Serial.print("Month name: ");
-  Serial.println(currentMonthName);
-
-  int currentYear = ptm->tm_year+1900;
-  Serial.print("Year: ");
-  Serial.println(currentYear);
-
-  //Print complete date:
-  String currentDate = String(currentYear) + "-" + String(currentMonth) + "-" + String(monthDay);
-  Serial.print("Current date: ");
-  Serial.println(currentDate);
-
-  Serial.println("");
-
-  delay(2000);
+int taskId=0;
+void testLeds(){
+  taskId++;
+  if(taskId>3) taskId=0;
+  leds[0] = color[taskId]; 
+  FastLED.show();
+  delay(5000);
 }
