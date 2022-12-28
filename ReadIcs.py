49e8cb8442f974ebefa8bb6794aabb3605faa4e0
tobiwern <tobiwern@gmail.com>
2022-12-28 11:03:04 +0100

    Combining multiple ics, auto-assign color

--- ReadIcs.py
+++ ReadIcs.py
@@ -2,48 +2,58 @@
 from icalendar import Calendar, Event, vDatetime
 import datetime
 from pytz import UTC # timezone
+import re
 
-file = "C:/Users/tobiw/Downloads/Abfuhrkalender-Hirrlingen-2023.ics"
-#file = "C:/Users/tobiw/Downloads/allestrassenweilimschoenbuch.ics"
-#file = "C:/Users/tobiw/Downloads/allestrassenebhausen.ics"
-#file = "C:/Users/tobiw/Downloads/Abfallkalender Hirrlingen.ics"
+#files = ["C:/Users/tobiw/Downloads/Abfuhrkalender-Hirrlingen-2023.ics","C:/Users/tobiw/Downloads/Abfuhrkalender-Hirrlingen-2022.ics", "C:/Users/tobiw/Downloads/Abfallkalender Hirrlingen.ics"]
+#files = ["C:/Users/tobiw/Downloads/allestrassenweilimschoenbuch.ics"]
+files = ["C:/Users/tobiw/Downloads/allestrassenebhausen.ics"]
 #validEntries = ['Altpapier-Tonne in Hirrlingen', 'Bioabfall in Hirrlingen', 'Gelber Sack in Hirrlingen', 'Häckselgut in Hirrlingen', 'Restmüll in Hirrlingen']
 #validEntries = ['Altpapier-Tonne in Hirrlingen', 'Gelber Sack in Hirrlingen', 'Häckselgut in Hirrlingen', 'Restmüll in Hirrlingen']
 validEntries = []
-colors = ['gelb', 'grün', 'weiß']
+colorDict = {'PAPIER':'0x0000FF', 'BIO,HÄCKSEL':'0x00FF00', 'GELB,WERT':'0xFFFF00', 'REST':'0xFFFFFF'}
 dateDict = {}
 items = set()
-with open(file,'rb') as g:
-    gcal = Calendar.from_ical(g.read())
-    for component in gcal.walk():
-    #   print(component)
-        if component.name == "VEVENT":
-            date = str(component.get('dtstart').dt)
-            print(date)
-            epoche = int((component.get('dtstart').dt-datetime.date(1970,1,1)).total_seconds())
-            print(epoche)
-            #date1 = datetime.date(1970,1,1)
-            item = str(component.get('summary'))
-            if((not validEntries) or (item in validEntries)):
-                items.add(item)
-                dateDict.setdefault(epoche, []).append(item) 
-print(dateDict)
-#for entry in dateDict.values:
-#    if(len(entry)>1):
-#        print(entry)
+for file in files:
+    with open(file,'rb') as g:
+        gcal = Calendar.from_ical(g.read())
+        for component in gcal.walk():
+        #   print(component)
+            if component.name == "VEVENT":
+                date = str(component.get('dtstart').dt)
+                #print(date)
+                epoche = int((component.get('dtstart').dt-datetime.date(1970,1,1)).total_seconds())
+                #print(epoche)
+                #date1 = datetime.date(1970,1,1)
+                item = str(component.get('summary'))
+                if((not validEntries) or (item in validEntries)):
+                    items.add(item)
+                    dateDict.setdefault(epoche, []).append(item) 
+#print(dateDict)
 
 items = list(sorted(items))
-#print("std::unordered_map<int,int>epochTaskDict = {")
 print("#include <unordered_map>")
 print("std::unordered_map<int,String>epochTaskDict = {")
-for entry in dateDict:
+for entry in sorted(dateDict.keys()):
     values = []
     for item in dateDict[entry]:
         i = items.index(item)
         values.append(str(i))
-#    print("{{{},{}}},".format(entry,",".join(values)))
-    print("{{{},\"{}\"}},".format(entry,",".join(values)))
+    print("{{{},\"{}\"}}, //{} {}".format(entry,",".join(values),datetime.datetime.fromtimestamp(entry).strftime('%d.%m.%Y'), dateDict[entry]))
 print("};")
 print("const String task[] = {{\"{}\"}};".format("\", \"".join(items)))
 
-print(len(dateDict))
+def getMatchingColor(item):
+    for key in colorDict.keys():
+        for entry in key.split(","):
+            if(re.search(entry, item.upper())):
+                return(colorDict[key])
+    print("WARNING: No matching color detected for item {}.".format(item))
+
+colors = []
+for item in items:
+    colors.append(getMatchingColor(item))    
+
+print("const int color[] = {{{}}};".format(",".join(colors)))
+print("const int validIndex[] = {{{}}};".format(",".join("%d" %i for i in range(0,len(items))))) #keine Altpapiertonne, kein Bioabfall, (aber Papier, Pappe Kartonagen von Bogenschütz)
+
+print("Number of entries " + str(len(dateDict)))
