3560c58b9ae9def6d051f83c5db3dee0131a4f94
tobiwern <tobiwern@gmail.com>
2022-12-30 21:35:38 +0100

    reformatting, added ConfigPage
    Only updating start/endHour.
    Parsing ICS done. ToDo: create JSON and save to LittleFS.

--- TrashReminder.ino
+++ TrashReminder.ino
@@ -15,19 +15,22 @@ Epoch Converter: https://www.epochconverter.com/
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
+#include <ESP8266WebServer.h>
+ESP8266WebServer server(80);
+#include "webpage.h"
 #include "data_neuweiler.h"
 
-int colorIds[]     = {-1,-1,-1}; //allow max three tasks on the same day
-int colorIdsLast[] = {-1,-1,-1};
-int numberOfColorIds = sizeof(colorIds)/sizeof(colorIds[0]);
-int colorIndex = 0; //used to toggle between multiple colors for same day tasks
+int colorIds[] = { -1, -1, -1 };  //allow max three tasks on the same day
+int colorIdsLast[] = { -1, -1, -1 };
+int numberOfColorIds = sizeof(colorIds) / sizeof(colorIds[0]);
+int colorIndex = 0;  //used to toggle between multiple colors for same day tasks
 
-int startTime = 15*60*60; //seconds => 15 Uhr
-int endTime = (24+8)*60*60; //seconds => nächster Tag 8 Uhr morgens
-int brightness = 255; //highest value since used to fadeBy...
-int fadeAmount = 5;  // Set the amount to fade to 5, 10, 15, 20, 25 etc even up to 255.
-int showDuration = 5000; //ms Splash screen
+int startHour = 15;       //am Vortag
+int endHour = 8;          //am Abholugstag
+int brightness = 255;     //highest value since used to fadeBy...
+int fadeAmount = 5;       // Set the amount to fade to 5, 10, 15, 20, 25 etc even up to 255.
+int showDuration = 5000;  //ms Splash screen
 unsigned long millisLast = 0;
 
 #define STATE_INIT 0
@@ -38,257 +41,320 @@ unsigned long millisLast = 0;
 
 int STATE = STATE_INIT;
 int STATE_PREVIOUS = -1;
-String stateTbl[] = {"STATE_INIT", "STATE_SHOW", "STATE_DISCONNECTED", "STATE_QUERY", "STATE_CONFIGURE"};
+int STATE_NEXT = -1;
+String stateTbl[] = { "STATE_INIT", "STATE_SHOW", "STATE_DISCONNECTED", "STATE_QUERY", "STATE_CONFIGURE" };
 
 #include <FastLED.h>
 #define NUM_LEDS 1
-#define DATA_PIN 4 //D2
-#define BRIGHTNESS         0
-#define FRAMES_PER_SECOND  120
+#define DATA_PIN 4  //D2
+#define BRIGHTNESS 0
+#define FRAMES_PER_SECOND 120
 CRGB leds[NUM_LEDS];
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+uint8_t gHue = 0;  // rotating "base color" used by many of the patterns
 
 //Switch
 #include <Button.h>
-#define REED_PIN 5 //D1
+#define REED_PIN 5  //D1
 Button reed(REED_PIN, LOW, BTN_PULLUP, LATCHING);
 int switchCounter = 0;
-int multiClickTimeout = 1000; //ms
+int multiClickTimeout = 1000;  //ms
 unsigned long lastSwitchMillis = 0;
 
-int acknowledge = 0; //turn off reminder light when the Mülleimer is lupft
-int triggerEpoch = 0; //used to detect if the epochDict is changing => reset acknowledge
-int initialized = 0; //in order to prevent acknowledge to be triggered at the beginning
+int acknowledge = 0;   //turn off reminder light when the Mülleimer is lupft
+int triggerEpoch = 0;  //used to detect if the epochDict is changing => reset acknowledge
+int initialized = 0;   //in order to prevent acknowledge to be triggered at the beginning
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
-int nowEpoch = 0; //global since only querying every minute
-int queryIntervall = 60000; //ms => every minute (could be less, however to really turn on LED at intended time...) 
+int nowEpoch = 0;            //global since only querying every minute
+int queryIntervall = 60000;  //ms => every minute (could be less, however to really turn on LED at intended time...)
 unsigned long lastQueryMillis = 0;
- 
+
 void setup() {
   Serial.begin(115200);
   reed.attachEdgeDetect(doNothing, setAcknowledge);
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
-  leds[0] = CRGB::Red; //in case no successful WiFi connection
+  leds[0] = CRGB::Red;                                      //in case no successful WiFi connection
   FastLED.show();
+  WiFiManager wm;
+  //  wm.resetSettings();
+  if (wm.autoConnect("TrashReminder")) {
+    Serial.println("Successfully connected.");
+  } else {
+    Serial.println("Failed to connect.");
+  }
   WiFi.hostname("TrashReminder");
-  WiFiManager wm; 
-//  wm.resetSettings();
-   if(wm.autoConnect("TrashReminder")){
-     Serial.println("Successfully connected.");     
-   } else {
-     Serial.println("Failed to connect.");
-   }
+  //Time Client
   timeClient.begin();
-  timeClient.setTimeOffset(3600); //GMT+1
+  timeClient.setTimeOffset(3600);  //GMT+1
 }
 
 void loop() {
   handleState();
 }
 
-void handleConnection(){
-  if(!WiFi.isConnected()){
+// ESP Functions
+void handleConnection() {
+  if (!WiFi.isConnected()) {
     Serial.println("Connection lost!");
-    STATE = STATE_DISCONNECTED;
-  } 
+    STATE_NEXT = STATE_DISCONNECTED;
+  }
 }
 
-int getCurrentTimeEpoch(){
+int getCurrentTimeEpoch() {
   timeClient.update();
-  return(timeClient.getEpochTime());
+  return (timeClient.getEpochTime());
 }
 
-void resetColorIds(){
+void resetColorIds() {
   memset(colorIds, -1, sizeof(colorIds));
 }
 
-void printColorIds(){
-  for (int i=0; i<sizeof colorIds/sizeof colorIds[0]; i++) {
-    Serial.println("colorIds[" + String(i) + "] = " + String(colorIds[i])); 
-  }     
+void printColorIds() {
+  for (int i = 0; i < sizeof colorIds / sizeof colorIds[0]; i++) {
+    Serial.println("colorIds[" + String(i) + "] = " + String(colorIds[i]));
+  }
 }
-void handleLed(int nowEpoch){
+void handleLed(int nowEpoch) {
   int dictEpoch;
   resetColorIds();
-//  printColorIds();
-  for (auto entry :epochTaskDict)  {
+  //  printColorIds();
+  for (auto entry : epochTaskDict) {
     dictEpoch = entry.first;
-//    Serial.println("now: " + String(nowEpoch) + ", dictEpoch = " + String(dictEpoch) + ", dictEpoch+startTime = " + String(dictEpoch+startTime)+ ", dictEpoch+endTime = " + String(dictEpoch+endTime));
-    if((nowEpoch > dictEpoch-24*60*60+startTime) && (nowEpoch < dictEpoch-24*60*60+endTime)){
-      if(dictEpoch != triggerEpoch){
-        acknowledge = 0; //acknowledge only valid for same triggerEpoch
+    //    Serial.println("now: " + String(nowEpoch) + ", dictEpoch = " + String(dictEpoch) + ", dictEpoch+startTime = " + String(dictEpoch+startTime)+ ", dictEpoch+endTime = " + String(dictEpoch+endTime));
+    if ((nowEpoch > dictEpoch + (startHour - 24) * 60 * 60) && (nowEpoch < dictEpoch + endHour * 60 * 60)) {
+      if (dictEpoch != triggerEpoch) {
+        acknowledge = 0;  //acknowledge only valid for same triggerEpoch
         colorIndex = 0;
         Serial.println("Resetting since new trigger!");
-      } 
+      }
       triggerEpoch = dictEpoch;
-      if(!acknowledge){
+      if (!acknowledge) {
         setColorIds(entry.second);
       }
-    }    
+    }
   }
-//  printColorIds();
+  //  printColorIds();
   setTaskColor();
   FastLED.show();
 }
 
-boolean isValidTask(int taskId){
-  for(int i = 0; i < sizeof(validIndex) / sizeof(validIndex[0]); i++){
-    if(taskId == validIndex[i]){return(true);}
+boolean isValidTask(int taskId) {
+  for (int i = 0; i < sizeof(validIndex) / sizeof(validIndex[0]); i++) {
+    if (taskId == validIndex[i]) { return (true); }
   }
-  return(false);
+  return (false);
 }
 
-void setColorIds(String taskIds){
-  int start = 0, index = 0, indexFound, taskId; 
-  do{
-    indexFound = taskIds.indexOf(",", start); 
+void setColorIds(String taskIds) {
+  int start = 0, index = 0, indexFound, taskId;
+  do {
+    indexFound = taskIds.indexOf(",", start);
     taskId = taskIds.substring(start, indexFound).toInt();
-    if(isValidTask(taskId)){colorIds[index++] = taskId;}
+    if (isValidTask(taskId)) { colorIds[index++] = taskId; }
     Serial.println("taskId = " + String(taskId) + ", start = " + String(start) + ", indexFound = " + String(indexFound) + ", validTask = " + String(isValidTask(taskId)));
-    start=indexFound+1;    
+    start = indexFound + 1;
   } while (indexFound != -1);
 }
 
-void handleReed(){
-  reed.update();  
+void handleReed() {
+  reed.update();
   initialized = true;
 }
 
-void setAcknowledge(){
+void setAcknowledge() {
   unsigned long now = millis();
-  if(initialized){
-    Serial.println("OFF - Acknowedge!");
+  if (initialized) {
+    //    Serial.println("OFF - Acknowedge!");
     acknowledge = 1;
     //Multi-click detection
     switchCounter++;
-    if(((now-lastSwitchMillis)>multiClickTimeout) || (lastSwitchMillis == 0)){switchCounter=0;}
-    Serial.println("Counter = " + String(switchCounter));
+    if (((now - lastSwitchMillis) > multiClickTimeout) || (lastSwitchMillis == 0)) { switchCounter = 0; }
+    //    Serial.println("Counter = " + String(switchCounter));
     lastSwitchMillis = now;
-    if(switchCounter == 2){
-      if(STATE == STATE_CONFIGURE){
-        STATE = STATE_INIT; //reset
+    if (switchCounter == 2) {
+      if (STATE == STATE_CONFIGURE) {
+        STATE_NEXT = STATE_INIT;  //reset
       } else {
-        STATE = STATE_CONFIGURE;
+        STATE_NEXT = STATE_CONFIGURE;
       }
     }
-  }  
+  }
 }
 
-void doNothing(){
-  Serial.println("ON");
-  true;  
+void doNothing() {
+  //  Serial.println("ON");
+  true;
 }
 
-void incrementColorId(){
+void incrementColorId() {
   int numberOfTasks = 0;
-  for(int i=0;i<numberOfColorIds;i++){
-    if(colorIds[i] !=-1){numberOfTasks++;}
+  for (int i = 0; i < numberOfColorIds; i++) {
+    if (colorIds[i] != -1) { numberOfTasks++; }
   }
   colorIndex++;
-  if(colorIndex >= numberOfTasks){colorIndex = 0;}
-//  Serial.println("colorIndex = " + String(colorIndex) + ", numberOfTasks = " + String(numberOfTasks) + ", numberOfColorIds = " + String(numberOfColorIds));
+  if (colorIndex >= numberOfTasks) { colorIndex = 0; }
+  //  Serial.println("colorIndex = " + String(colorIndex) + ", numberOfTasks = " + String(numberOfTasks) + ", numberOfColorIds = " + String(numberOfColorIds));
 }
 
-void setBrightness(int blinkSpeed=20, boolean reset=false){
-  if(reset){
+void setBrightness(int blinkSpeed = 20, boolean reset = false) {
+  if (reset) {
     brightness = 255;
   } else {
-    if((colorIds[0] != colorIdsLast[0]) || (colorIds[1] != colorIdsLast[1])){brightness = 255;}
+    if ((colorIds[0] != colorIdsLast[0]) || (colorIds[1] != colorIdsLast[1])) { brightness = 255; }
     colorIdsLast[0] = colorIds[0];
     colorIdsLast[1] = colorIds[1];
   }
   leds[0].fadeLightBy(brightness);
   brightness = brightness + fadeAmount;
-  if(brightness<=0){brightness = 0; }
-  if(brightness>=255){brightness = 255; incrementColorId();}    
-//    Serial.println("Brightness = " + String(brightness) + ", ColorIndex = " + String(taskId));
-  if(brightness <= 0 || brightness >= 255){fadeAmount = -fadeAmount;} 
-  delay(blinkSpeed);   
+  if (brightness <= 0) { brightness = 0; }
+  if (brightness >= 255) {
+    brightness = 255;
+    incrementColorId();
+  }
+  //    Serial.println("Brightness = " + String(brightness) + ", ColorIndex = " + String(taskId));
+  if (brightness <= 0 || brightness >= 255) { fadeAmount = -fadeAmount; }
+  delay(blinkSpeed);
 }
 
-void setTaskColor(){
-  if(colorIds[0] == -1){
-//    Serial.println("No Event");
+void setTaskColor() {
+  if (colorIds[0] == -1) {
+    //    Serial.println("No Event");
     leds[0] = CRGB::Black;
   } else {
     // EVENT
-//    Serial.println("Event: " + task[taskId] + " => LED = " + String(color[taskId],HEX));  
+    //    Serial.println("Event: " + task[taskId] + " => LED = " + String(color[taskId],HEX));
     leds[0] = color[colorIds[colorIndex]];
-    setBrightness();  
+    setBrightness();
   }
 }
 
-void setColor(int color, boolean fade = true, int blinkSpeed=20){
+void setColor(int color, boolean fade = true, int blinkSpeed = 20) {
   leds[0] = color;
-  if(fade){setBrightness(blinkSpeed);}
+  if (fade) { setBrightness(blinkSpeed); }
   FastLED.show();
 }
 
 /// Splash Screen
-void rainbow() 
-{
+void rainbow() {
   // FastLED's built-in rainbow generator
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+  fill_rainbow(leds, NUM_LEDS, gHue, 7);
 }
 
-void rainbowWithGlitter() 
-{
+void rainbowWithGlitter() {
   // built-in FastLED rainbow, plus some random sparkly glitter
   rainbow();
   addGlitter(50);
 }
 
-void addGlitter( fract8 chanceOfGlitter) 
-{
-  if( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White;
+void addGlitter(fract8 chanceOfGlitter) {
+  if (random8() < chanceOfGlitter) {
+    leds[random16(NUM_LEDS)] += CRGB::White;
   }
 }
 
-void handleState(){
+// Server Functions
+void startWebServer() {
+  Serial.println("Starting WebServer...");
+  server.on("/", handleRoot);
+  server.on("/set_start", setStartHour);
+  server.on("/set_end", setEndHour);
+  server.on("/settings", sendSettings);
+  server.onNotFound(notFound);
+  server.begin();
+  Serial.print("IP address: ");
+  Serial.print(WiFi.localIP());
+  Serial.print(", hostName: ");
+  Serial.println(WiFi.getHostname());
+}
+
+void sendSettings() {
+  String value;
+  value = String(startHour) + "," + String(endHour);
+  Serial.println("Sending settings: " + value);
+  server.send(200, "text/plane", value);
+}
+
+void stopWebServer() {
+  Serial.println("Stopping WebServer...");
+  server.stop();
+}
+
+void notFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+void handleRoot() {
+  String s = webpage;
+  server.send(200, "text/html", s);
+}
+
+void setStartHour() {
+  String hour = server.arg("value");
+  Serial.println("Setting startHour = " + hour);
+  startHour = hour.toInt();
+  setColor(CRGB::White, false);
+  setColor(CRGB::Purple, false);
+}
+
+void setEndHour() {
+  String hour = server.arg("value");
+  Serial.println("Setting endHour = " + hour);
+  endHour = hour.toInt();
+  setColor(CRGB::White, false);
+  setColor(CRGB::Purple, false);
+}
+
+void handleState() {
   unsigned long millisNow = millis();
-  if(STATE != STATE_PREVIOUS) {Serial.println("STATE = " + stateTbl[STATE]);}
+  STATE_NEXT = -1;
+  //  Serial.println("STATE = " + stateTbl[STATE] + ", STATE_PREVIOUS = " + stateTbl[STATE_PREVIOUS]);
+  if (STATE != STATE_PREVIOUS) { Serial.println("STATE = " + stateTbl[STATE]); }
   switch (STATE) {
-    case STATE_INIT:                //***********************************************************
+    case STATE_INIT:  //***********************************************************
+      if (STATE_PREVIOUS == STATE_CONFIGURE) { stopWebServer(); }
       millisLast = millisNow;
       brightness = 255;
       colorIndex = 0;
       acknowledge = 0;
       resetColorIds();
-      STATE = STATE_SHOW;
+      STATE_NEXT = STATE_SHOW;
       break;
-    case STATE_SHOW:                //***********************************************************
+    case STATE_SHOW:  //***********************************************************
       rainbowWithGlitter();
-      FastLED.show();  
+      FastLED.show();
       // insert a delay to keep the framerate modest
-      FastLED.delay(1000/FRAMES_PER_SECOND); 
+      FastLED.delay(1000 / FRAMES_PER_SECOND);
       // do some periodic updates
-      EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-      if((millisNow-millisLast) > showDuration){STATE = STATE_QUERY;}
+      EVERY_N_MILLISECONDS(20) {
+        gHue++;
+      }  // slowly cycle the "base color" through the rainbow
+      if ((millisNow - millisLast) > showDuration) { STATE_NEXT = STATE_QUERY; }
       break;
-    case STATE_DISCONNECTED:        //***********************************************************
+    case STATE_DISCONNECTED:  //***********************************************************
       setColor(CRGB::Red, true, 2);
-      if(WiFi.isConnected()){STATE = STATE_INIT;}
-      break;  
-    case STATE_QUERY:               //***********************************************************
-      if(((millisNow-lastQueryMillis) > queryIntervall) || (lastQueryMillis == 0)){
+      if (WiFi.isConnected()) { STATE_NEXT = STATE_INIT; }
+      break;
+    case STATE_QUERY:  //***********************************************************
+      if (((millisNow - lastQueryMillis) > queryIntervall) || (lastQueryMillis == 0)) {
         nowEpoch = getCurrentTimeEpoch();
         Serial.println("Received current epoch time: " + String(nowEpoch));
         lastQueryMillis = millisNow;
-      } 
-      handleLed(nowEpoch); 
+      }
+      handleLed(nowEpoch);
       handleReed();
       handleConnection();
-      break; 
+      break;
     case STATE_CONFIGURE:
+      if (STATE_PREVIOUS == STATE_QUERY) { startWebServer(); }
       setColor(CRGB::Purple, false);
+      server.handleClient();
       handleReed();
-      break;       
+      break;
     default:
       break;
-  }   
-  STATE_PREVIOUS = STATE; 
+  }
+  STATE_PREVIOUS = STATE;
+  if (STATE_NEXT != -1) { STATE = STATE_NEXT; }
 }
--- webpage.h
+++ webpage.h
@@ -1,66 +1,213 @@
-//=========================================
-//HTML + CSS + JavaScript codes for webpage
-//=========================================
-const char webpageCode[] =
-R"=====(
+const char webpage[] PROGMEM = R"=====(
 <!DOCTYPE html>
-<html>
-<!------------------------------C S S--------------------------------->
-<head>
-    <style>
-        #btn
-        {
-          display: inline;
+  <html>
+    <head>
+      <style type="text/css">
+        .button {
+          background-color: #4CAF50;
+          /* Green */
+          border: none;
+          color: white;
+          padding: 15px 32px;
+          text-align: center;
           text-decoration: none;
-          background: red;
-          color:rgba(0, 0, 255, 0.7);
-          font: 80px calibri;
-          border-radius: 50%;
-          box-shadow: 0px 0px 10px 15px #000000;
-          border: solid 10px rgba(255, 255, 0, 0.9);
-          cursor: pointer;
+          display: inline-block;
+          font-size: 16px;
         }
-        body {font-family: "Calibri"; background-color: grey}
-        h1   {color: whitesmoke; text-align:center; font-size: 50px;}
-    </style>
-</head>
-<!----------------------------H T M L--------------------------------->
-<body>
-    <h1>
-        ESP8266 Webpage <br> (HTML + CSS + JavaScript) <br><br>
-        <a href="#" id="btn" ONCLICK='JS1()'>JS1</a> <br><br>
-        <a href="#" id="btn" ONCLICK='JS2()'>JS2</a> <br><br>
-        <a href="#" id="btn" ONCLICK='JS3()'>JS3</a>
-    </h1>
-<!---------------------------JavaScript------------------------------->
-<script>
-    //function displays text message
-    let m1 = 'JavaScript is an object-oriented language'
-    let m2 = ' that creates interactive effects within web browsers.'
-    let m3 = ' The code syntax of JavaScript is similar to C++.'
-    function JS1()
-    {
-      alert(m1 + m2 + m3);
-    }
-    //function prompts for name, then displays message
-    function JS2()
-    {
-        let yourName;
-        do
-        {
-            yourName = prompt("Who are you?");
-        } while (!yourName);
-        alert('Hello ' + yourName);
-    }
-    //function computes factorial of integer number
-    function JS3()
-    {
-        let f = 1;
-        let n = prompt('FACTORIAL\nEnter positive integer number: ');
-        for(let i=1; i<=n; i++) f = f * i;
-        alert('Factorial = ' + f);
-    }   
-</script>
+
+        body {
+          text-align: center;
+        }
+
+        table {
+          width: 80%;
+          text-align: center;
+          margin-left: auto;
+          margin-right: auto;
+        }
+
+        div.frame {
+          border: 2px solid  #4CAF50;
+          border-radius: 8px;
+          padding: 5px;
+          width: 350px;
+          margin-left: auto;
+          margin-right: auto;
+
+        }
+
+        td.value {
+          white-space: nowrap;
+          vertical-align: center;
+          text-align: left;
+        }
+
+        td.description {
+          vertical-align: center;
+          text-align: right;
+        }
+      </style>
+      <script>
+    /*    function send(led_sts){
+          var xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              document.getElementById("state").innerHTML = this.responseText;
+            }
+          };
+          xhttp.open("GET", "led_set?state="+led_sts, true);
+          xhttp.send();
+        }
+    
+        //setInterval(function(){getData();}, 2000);
+        */
+
+        function requestSettings() {
+          var xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              value = this.responseText;
+              tokens = value.split(",");
+              document.getElementById("start").value = tokens[0];
+              document.getElementById("end").value = tokens[1];
+            }
+          };
+          xhttp.open("GET", "settings", true);
+          xhttp.send();
+        }
+       
+
+        document.addEventListener('DOMContentLoaded', function () {
+    	    enableEventListener('start');
+          enableEventListener('end');
+        });
+        function enableEventListener(dropdown) {
+          document.getElementById(dropdown).addEventListener('change', function(){sendUpdate(dropdown);});
+        }
+	      function sendUpdate(dropdown) {
+          var value = document.getElementById(dropdown).value;
+          var xhttp = new XMLHttpRequest();
+          xhttp.open("GET", "set_" + dropdown+ "?value=" + value, true);
+          xhttp.send();          
+        }
+        /// ICS/iCAL Processing
+        function processFiles() {
+            var dateDict = {};
+            var items = [];
+            console.log(items);
+            var files = document.getElementById('files').files;
+            for (var fileIndex = 0; fileIndex < files.length; fileIndex++) {
+                var file = files[fileIndex];
+                var reader = new FileReader();
+                reader.onload = function (progressEvent) {
+                    const text = this.result; //entire file
+                    var lines = text.split('\n');
+                    for (var i = 0; i < lines.length; i++) {
+                        line = lines[i];
+                        if (line.search("DTSTART") != -1) {
+                            var date = line.split(":")[1];
+                            var epoch = new Date(date.substring(0, 4) + "-" + date.substring(4, 6) + "-" + date.substring(6, 8)).getTime()
+                        } else if (line.search("SUMMARY") != -1) {
+                            var item = line.split(":")[1];
+                        } else if (line.search("END:VEVENT") != -1) {
+                            if (!(epoch in dateDict)) { dateDict[epoch] = []; }
+                            var arr = dateDict[epoch];
+                            arr.push(item);
+                            dateDict[epoch] = arr;
+                            if (!items.includes(item)) {
+                                items.push(item);
+                                genCheckboxes(items);
+                            }
+                        }
+                    }
+                };
+                reader.readAsText(file);
+            }
+        }
+
+        function genCheckboxes(items) {
+            console.log(items);
+            var i = 0;
+            var text = "<br>Bitte w&auml;hlen Sie die Abfallarten aus, an die Sie erinnert werden wollen:<br>";
+            text += "<table><tr><td class=value>"            
+            for (let i = 0; i < items.length; i++) {
+                text += "<div><input type='checkbox' id='task" + i + "' name=task'" + i + "' checked>";
+                text += "<label for='task" + i + "'>" + items[i] + "</label><div>";
+            }
+            text += "</td></tr></table>"            
+            document.getElementById("tasks").innerHTML = text
+        }
+      </script>
+    </head>  
+
+   <body onload='requestSettings()')>
+     <img src='https://github.com/tobiwern/TrashReminder/blob/main/pictures/TrashReminder.jpg?raw=true' alt='Trash Reminder' width='400' height='185'>
+  <h1>M&uuml;ll-Erinnerung Einstellungen</h1>
+  <form name='config'>
+    <div class=frame>
+      <h2>Zeitpunkt der Erinnerung</h2>
+      <table>
+        <tr>
+          <td class=description><label for="start">Start der Erinnerung<br>(am Vortag der Abholung):</label></td>
+          <td class=value><select id="start" name="start">
+              <option value="10">10 Uhr</option>
+              <option value="11">11 Uhr</option>
+              <option value="12">12 Uhr</option>
+              <option value="13">13 Uhr</option>
+              <option value="14">14 Uhr</option>
+              <option value="15">15 Uhr</option>
+              <option value="16">16 Uhr</option>
+              <option value="17">17 Uhr</option>
+              <option value="18">18 Uhr</option>
+              <option value="19">19 Uhr</option>
+              <option value="19">20 Uhr</option>
+              <option value="19">21 Uhr</option>
+              <option value="19">22 Uhr</option>
+              <option value="19">23 Uhr</option>
+            </select></td>
+        </tr>
+        <tr>
+          <td class=description><label class='fancy-input' for="end">Ende der Erinnerung<br>(am Tag der
+              Abholung):</label></td>
+          <td class=value><select id="end" name="end">
+              <option value="6">6 Uhr</option>
+              <option value="7">7 Uhr</option>
+              <option value="8">8 Uhr</option>
+              <option value="9">9 Uhr</option>
+              <option value="10">10 Uhr</option>
+              <option value="11">11 Uhr</option>
+              <option value="12">12 Uhr</option>
+              <option value="13">13 Uhr</option>
+              <option value="14">14 Uhr</option>
+              <option value="15">15 Uhr</option>
+              <option value="16">16 Uhr</option>
+              <option value="17">17 Uhr</option>
+              <option value="18">18 Uhr</option>
+              <option value="19">19 Uhr</option>
+            </select></td>
+        </tr>
+      </table>
+      <br>
+    </div>
+    <br>
+    <div class=frame>
+      <h2>Abfuhrtermine (ICS/ICAL)</h2>
+      <table>
+        <tr><td><label for="start">W&auml;hlen Sie eine oder mehrere bereits heruntergeladene ICS oder ICAL Dateien ihres Entsorgungsunternehmens aus:</label></td></tr>
+        <tr><td><input type="file" name="files" id="files" accept=".ics" onchange="processFiles()" multiple></td></tr>
+        <tr><td><div id='tasks'></div></td></tr>  
+      </table>
+      <br>
+    </div>    
+    
+
+  </form>
+  <br>
+  <div>
+    <button class="button" onclick="send(1)">Erinnerung ausschalten</button>
+    <button class="button" onclick="send(0)">Erinnerung einschalten</button>
+    <button class="button" onclick="send(2)">Feuerwerk</button>
+  </div>
 </body>
-</html>
+  </html>
 )=====";
