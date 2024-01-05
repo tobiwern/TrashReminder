d3805470e14478e55f91bcb387f2931b35d6cffa
tobiwern <tobiwern@gmail.com>
2024-01-05 12:00:26 +0100

    Instant webpage response (task/hour updates)
    initDataFromFile: Resetting numberOfEpoch etc.
    writeStartEndTimes: Added initStartEndTimes()
    receiveFromWebpage_Tasks/deleteTasks: Added initDataFromFile()
    fireworks: STATE_FOLLOWING = STATE_QUERY
    receiveFromWebpage_Acknowledge: new
    isClientConnected: new (not used)
    Serial => DEBUG_SERIAL

--- webserver.h
+++ webserver.h
@@ -13,52 +13,52 @@ boolean serverRunning = false;
 #define JSON_MEMORY 1024 * 30
 
 void printTaskIds(int taskIds[]) {  //debug
-  Serial.print("taskIds[] = ");
+  DEBUG_SERIAL.print("taskIds[] = ");
   for (int i = 0; i < maxNumberOfTasksPerDay; i++) {
-    if (taskIds[i] != -1) { Serial.print(String(taskIds[i]) + " "); }
+    if (taskIds[i] != -1) { DEBUG_SERIAL.print(String(taskIds[i]) + " "); }
   }
-  Serial.println("");
+  DEBUG_SERIAL.println("");
 }
 
 boolean initDataFromFile() {
   if (!startLittleFS()) { return (""); }
-  Serial.printf("INFO: Reading file: %s\n", dataFile);
+  DEBUG_SERIAL.printf("INFO: Reading file: %s\n", dataFile);
   File file = LittleFS.open(dataFile, "r");
+  numberOfValidTaskIds = 0;
+  numberOfTaskIds = 0;
+  int numberOfColors = 0;  //only locally needed
+  numberOfEpochs = 0;
   if (!file) {
-    Serial.println("INFO: Failed to open file " + String(dataFile) + " for reading!");
+    DEBUG_SERIAL.println("INFO: Failed to open file " + String(dataFile) + " for reading!");
     return (false);
   }
   DynamicJsonDocument doc(JSON_MEMORY);  //on heap for large amount of data
   DeserializationError error = deserializeJson(doc, file);
   if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
+    DEBUG_SERIAL.print("deserializeJson() failed: ");
+    DEBUG_SERIAL.println(error.f_str());
     return (false);
   }
   // get validTasks ////////////////////////////////
   JsonArray validTaskIds = doc["validTaskIds"];  //Implicit cast
-  numberOfValidTaskIds = 0;
   for (JsonVariant v : validTaskIds) {
     validTaskId[numberOfValidTaskIds++] = v.as<int>();
-    Serial.println("validTaskId: " + String(validTaskId[numberOfValidTaskIds - 1]));
+    DEBUG_SERIAL.println("validTaskId: " + String(validTaskId[numberOfValidTaskIds - 1]));
   }
   // get tasks ////////////////////////////////
   JsonArray tasks = doc["tasks"];  //Implicit cast
-  numberOfTaskIds = 0;
   for (String taskText : tasks) {  //Implicit cast
     task[numberOfTaskIds++] = taskText;
-    Serial.println("Task: " + task[numberOfTaskIds - 1]);
+    DEBUG_SERIAL.println("Task: " + task[numberOfTaskIds - 1]);
   }
   // get colors ////////////////////////////////
   JsonArray colors = doc["colors"];  //implicit cast to JsonArray!
-  numberOfTaskIds = 0;
   for (String colorText : colors) {
     unsigned long int color1 = strtoul(colorText.c_str(), NULL, 16);  //conversion from HEX String => HEX number
-    color[numberOfTaskIds++] = color1;
-    Serial.println("Color: " + colorText + ", value = " + String(color1));
+    color[numberOfColors++] = color1;
+    DEBUG_SERIAL.println("Color: " + colorText + ", value = " + String(color1));
   }
   JsonArray epochTasks = doc["epochTasks"];  //Implicit cast
-  numberOfEpochs = 0;
   for (JsonObject obj : epochTasks) {
     for (JsonPair p : obj) {
       int taskIds[maxNumberOfTasksPerDay];
@@ -70,7 +70,7 @@ boolean initDataFromFile() {
         if (counter < maxNumberOfTasksPerDay - 1) {  //prevent running over reserved memory
           taskIds[counter++] = taskId;
         } else {
-          Serial.println("WARNING: Too many taskIds: SKIPPING: " + taskId);
+          DEBUG_SERIAL.println("WARNING: Too many taskIds: SKIPPING: " + taskId);
         }
       }
       //JsonArray taskIdArray = p.value();                      // is a JsonVariant
@@ -81,9 +81,9 @@ boolean initDataFromFile() {
       if (numberOfEpochs < maxNumberOfEpochs - 1) {  //prevent running over reserved memory
         epochTaskDict[numberOfEpochs++] = entry;     //{ .epoch = epoch, .taskIds = taskIds };
       } else {
-        Serial.println("WARNING: Too many entries: SKIPPING: ");
+        DEBUG_SERIAL.println("WARNING: Too many entries: SKIPPING: ");
       }
-      Serial.print("epoch: " + String(epoch) + ", ");
+      DEBUG_SERIAL.print("epoch: " + String(epoch) + ", ");
       printTaskIds(taskIds);
     }
   }
@@ -92,21 +92,20 @@ boolean initDataFromFile() {
   return (true);
 }
 
-
 //settings
 void initStartEndTimes() {
   if (!startLittleFS()) { return; }
-  Serial.printf("INFO: Reading file: %s\n", settingsFile);
+  DEBUG_SERIAL.printf("INFO: Reading file: %s\n", settingsFile);
   File file = LittleFS.open(settingsFile, "r");
   if (!file) {
-    Serial.println("INFO: Failed to open file " + String(settingsFile) + " for reading!");
+    DEBUG_SERIAL.println("INFO: Failed to open file " + String(settingsFile) + " for reading!");
     return;
   }
   StaticJsonDocument<512> doc;
   DeserializationError error = deserializeJson(doc, file);
   if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
+    DEBUG_SERIAL.print("deserializeJson() failed: ");
+    DEBUG_SERIAL.println(error.f_str());
     return;
   }
   startHour = doc["startHour"];
@@ -118,8 +117,8 @@ void initStartEndTimes() {
 void sendTasksToWebpage() {  //transfering ESP data to the Webpage
   String value = readFile(dataFile);
   if (value != "") {
-    Serial.println("Received:" + value);
-    Serial.println("Sending taks: " + value);
+    DEBUG_SERIAL.println("Received:" + value);
+    DEBUG_SERIAL.println("Sending taks: " + value);
     server.send(200, "text/plane", value);
   } else {
     value = "ERROR";
@@ -130,12 +129,12 @@ void sendTasksToWebpage() {  //transfering ESP data to the Webpage
 void sendSettingsToWebpage() {  //transferring ESP settings => Webpage
   String value;
   value = String(startHour) + "," + String(endHour) + "," + maxNumberOfEpochs + "," + maxNumberOfTasksPerDay + "," + maxNumberOfTaskIds;
-  Serial.println("Sending settings: " + value);
+  DEBUG_SERIAL.println("Sending settings: " + value);
   server.send(200, "text/plane", value);
 }
 
 void notFound() {
-  Serial.println("Not found");
+  DEBUG_SERIAL.println("Not found");
   server.send(404, "text/plain", "Not found");
 }
 
@@ -147,13 +146,14 @@ void handleRoot() {
 void writeStartEndTimes() {
   String jsonText = "";
   jsonText = "{\"startHour\":" + String(startHour) + ",\"endHour\":" + String(endHour) + "}";
-  Serial.println("Writing settings: " + jsonText);
+  DEBUG_SERIAL.println("Writing settings: " + jsonText);
   writeFile("/settings.json", jsonText.c_str());
+  initStartEndTimes();
 }
 
 void setStartHour() {
   String hour = server.arg("value");
-  Serial.println("Setting startHour = " + hour);
+  DEBUG_SERIAL.println("Setting startHour = " + hour);
   startHour = hour.toInt();
   setColor(CRGB::White, false);
   setColor(CRGB::Purple, false);
@@ -163,7 +163,7 @@ void setStartHour() {
 
 void setEndHour() {
   String hour = server.arg("value");
-  Serial.println("Setting endHour = " + hour);
+  DEBUG_SERIAL.println("Setting endHour = " + hour);
   endHour = hour.toInt();
   setColor(CRGB::White, false);
   setColor(CRGB::Purple, false);
@@ -173,45 +173,48 @@ void setEndHour() {
 
 void receiveFromWebpage_Tasks() {
   String jsonText = server.arg("value");
-  Serial.println("Receiving settings in JSON format: " + jsonText);
+  DEBUG_SERIAL.println("Receiving settings in JSON format: " + jsonText);
   if (writeFile(dataFile, jsonText.c_str())) {
     server.send(200, "text/plane", "OK");
+    initDataFromFile();
   } else {
     server.send(500, "text/plane", "ERROR");
   }
 }
 
 void closeSettings() {
-  Serial.println("Closing Settings.");
+  DEBUG_SERIAL.println("Closing Settings.");
   STATE_NEXT = STATE_INIT;
 }
 
 void fireworks() {
-  Serial.println("Fireworks...");
-  STATE_FOLLOWING = STATE_CONFIGURE;
+  DEBUG_SERIAL.println("Fireworks...");
+  STATE_FOLLOWING = STATE_QUERY;
   STATE_NEXT = STATE_SHOW;
   millisLast = millis();                 //to reset show timer
   server.send(200, "text/plane", "OK");  //should always respond to prevent resend (10x)
 }
 
 void demo() {
-  Serial.println("Demo...");
+  DEBUG_SERIAL.println("Demo...");
   STATE_NEXT = STATE_DEMO;
   millisLast = millis();                 //to reset show timer
   server.send(200, "text/plane", "OK");  //should always respond to prevent resend (10x)
 }
 
 void deleteTasks() {
-  Serial.println("Delete Settings.");
+  DEBUG_SERIAL.println("Delete Settings.");
   if (deleteFile(dataFile)) {
     server.send(200, "text/plane", "OK");
+    initDataFromFile();
   } else {
     server.send(500, "text/plane", "ERROR");
   }
+  //STATE_NEXT = STATE_INIT;
 }
 
 void resetWifiSettings() {
-  Serial.println("Reset Wifi Settings.");
+  DEBUG_SERIAL.println("Reset Wifi Settings.");
   WiFiManager wm;  //Is this fine to have another instance?
   wm.resetSettings();
   server.send(200, "text/plane", "OK");
@@ -220,31 +223,43 @@ void resetWifiSettings() {
   ESP.restart();
 }
 
+void receiveFromWebpage_Acknowledge() {  //ToDo: Need to version webpages
+  DEBUG_SERIAL.println("Acknowledge...");
+  acknowledge = 1;
+  server.send(200, "text/plane", "OK");  //should always respond to prevent resend (10x)
+}
+
 void startWebServer() {
-    Serial.println("Starting WebServer...");
-    server.on("/", handleRoot);
-    server.on("/set_start", setStartHour);
-    server.on("/set_end", setEndHour);
-    server.on("/request_settings", sendSettingsToWebpage);
-    server.on("/request_tasks", sendTasksToWebpage);     //ESP => webpage
-    server.on("/send_tasks", receiveFromWebpage_Tasks);  //webpage => ESP name
-    server.on("/delete_tasks", deleteTasks);
-    server.on("/close", closeSettings);
-    server.on("/fireworks", fireworks);
-    server.on("/demo", demo);
-    server.on("/reset_wifi_settings", resetWifiSettings);
-    //  server.on("/send_ValidTaskIds", receiveFromWebpage_ValidTaskIds);
-    server.onNotFound(notFound);
-    server.begin();
-    Serial.print("IP address: ");
-    Serial.print(WiFi.localIP());
-    Serial.print(", hostName: http://");
-    Serial.println(WiFi.getHostname());
-    serverRunning = true;
+  DEBUG_SERIAL.println("Starting WebServer...");
+  server.on("/", handleRoot);
+  server.on("/set_start", setStartHour);
+  server.on("/set_end", setEndHour);
+  server.on("/request_settings", sendSettingsToWebpage);
+  server.on("/request_tasks", sendTasksToWebpage);     //ESP => webpage
+  server.on("/send_tasks", receiveFromWebpage_Tasks);  //webpage => ESP name
+  server.on("/delete_tasks", deleteTasks);
+  server.on("/close", closeSettings);
+  server.on("/fireworks", fireworks);
+  server.on("/demo", demo);
+  server.on("/acknowledge", receiveFromWebpage_Acknowledge);
+  server.on("/reset_wifi_settings", resetWifiSettings);
+  //  server.on("/send_ValidTaskIds", receiveFromWebpage_ValidTaskIds);
+  server.onNotFound(notFound);
+  server.begin();
+  DEBUG_SERIAL.print("IP address: ");
+  DEBUG_SERIAL.print(WiFi.localIP());
+  DEBUG_SERIAL.print(", hostName: http://");
+  DEBUG_SERIAL.println(WiFi.getHostname());
+  serverRunning = true;
 }
 
 void stopWebServer() {
-  Serial.println("Stopping WebServer...");
+  DEBUG_SERIAL.println("Stopping WebServer...");
   server.stop();
   serverRunning = false;
+}
+
+boolean isClientConnected() {
+  WiFiClient myclient = server.client();
+  return (myclient.connected());
 }
